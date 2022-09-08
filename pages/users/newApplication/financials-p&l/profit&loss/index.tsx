ec84d1c219aedf0cec287e1a9317544ab26e37ee
icodex <icodex.az@gmail.com>
2022-09-08 22:15:27 +0530

    feat: sidebar

--- pages/users/newApplication/financials-p&l/profit&loss/index.tsx
+++ pages/users/newApplication/financials-p&l/profit&loss/index.tsx
@@ -1,12 +1,12 @@
-import ApplicationLayout from '@components/layouts/new_application_layout'
-import React from 'react'
+import ApplicationLayout from "@components/layouts/new_application_layout";
+import React from "react";
 
 function index() {
-    return (
-        <ApplicationLayout>
-            <div>index</div>
-        </ApplicationLayout>
-    )
+  return (
+    <ApplicationLayout>
+      <div>index</div>
+    </ApplicationLayout>
+  );
 }
 
-export default index
+export default index;
