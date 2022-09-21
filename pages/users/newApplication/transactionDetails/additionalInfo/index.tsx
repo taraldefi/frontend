070f3e97ef7008e44568f30094c970459e5a21fb
icodex <icodex.az@gmail.com>
2022-09-21 16:52:18 +0530

    feat: ts-details contract

--- pages/users/newApplication/transactionDetails/additionalInfo/index.tsx
+++ pages/users/newApplication/transactionDetails/additionalInfo/index.tsx
@@ -4,7 +4,21 @@ import React from "react";
 function Index() {
   return (
     <ApplicationLayout>
-      <div className="plContainer">Additional Info</div>
+      <div className="tsNewContainers">
+        <div className="tsAddiContainer">
+          <div className="respContainer">
+            <div className="maintitle">RESPONSIBILITY</div>
+          </div>
+          <div className="vLine"></div>
+          <div className="addiContainer">
+            <div className="maintitle">ADDITIONAL INFO</div>
+          </div>
+          <div className="vLine"></div>
+          <div className="certiContainer">
+            <div className="maintitle">CERTIFICATE</div>
+          </div>
+        </div>
+      </div>
     </ApplicationLayout>
   );
 }
