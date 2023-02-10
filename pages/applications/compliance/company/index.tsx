fdad65f8a80141800bbd36455684fb4b5f99c4ea
icodex <icodex.az@gmail.com>
2023-02-10 19:19:29 +0530

    feat: move tables to ui library

--- pages/applications/compliance/company/index.tsx
+++ pages/applications/compliance/company/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import DashBoardLayout from "@components/layouts/dashboard_layout";
 import ComplianceLayout from "@components/layouts/compliance_layout";
-import { CompanyTable } from "@components/widgets/table/companyTable";
 import { TableData } from "./data";
-
+import { CompanyTable } from "@icodex-az/tariala-component-library";
 function Company() {
   return (
     <ComplianceLayout showexport={true}>
-      <CompanyTable TableData={TableData} />
+      <CompanyTable companyTableData={TableData} />
     </ComplianceLayout>
   );
 }
